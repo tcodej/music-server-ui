89a0d33ef37a0bcc9c08aaeea6e0b6a19b752540
Trent Johnson <trentj@netflixcontractors.com>
2024-02-19 13:04:34 -0800

    update sync script

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -e ssh --rsync-path="sudo rsync" -rpt --delete --progress /mnt/i/Audio/mp3/Music trentj@192.168.1.200:/mnt/usb/mp3
+rsync -e ssh --rsync-path="sudo rsync" -rpt --delete --progress /mnt/g/Audio/mp3/Music trentj@192.168.1.200:/mnt/usb/mp3
