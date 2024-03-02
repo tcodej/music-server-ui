c5374324b0dc77184e9d32100af1294aa78d0d46
Trent Johnson <trentj@netflixcontractors.com>
2024-03-02 11:08:17 -0800

    sass update, sync script

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 rsync -e ssh --rsync-path="sudo rsync" -rpt --delete --progress /mnt/g/Audio/mp3/Music trentj@192.168.1.200:/mnt/usb/mp3
+
+#sudo mount -t drvfs K: /mnt/k -o uid=$(id -u $USER),gid=$(id -g $USER),metadata
+#rsync -e ssh --rsync-path="sudo rsync" -rpt --delete --progress /mnt/g/Audio/mp3/Music /mnt/k/mp3
