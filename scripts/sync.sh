e31624d57e1fc1a2bc36d5b4895e511cd0fbdfb8
Trent Johnson <trentj@netflixcontractors.com>
2024-03-31 09:46:07 -0700

    update stmux

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rsync -e ssh --rsync-path="sudo rsync" -rptn --delete --progress /mnt/g/Audio/mp3/Music trentj@192.168.1.200:/mnt/usb/mp3
-#rsync -e ssh --rsync-path="sudo rsync" -rpt --delete --progress "/mnt/g/Audio/mp3/Music/The Tourists" trentj@192.168.1.200:/mnt/usb/mp3/Music
+# rsync -e ssh --rsync-path="sudo rsync" -rptn --delete --progress /mnt/g/Audio/mp3/Music trentj@192.168.1.200:/mnt/usb/mp3
+rsync -e ssh --rsync-path="sudo rsync" -rpt --delete --progress "/mnt/g/Audio/mp3/Music/The Cure" trentj@192.168.1.200:/mnt/usb/mp3/Music
 
 #sudo mount -t drvfs K: /mnt/k -o uid=$(id -u $USER),gid=$(id -g $USER),metadata
 #rsync -e ssh --rsync-path="sudo rsync" -rptn --delete --progress /mnt/g/Audio/mp3/Music /mnt/k/mp3
